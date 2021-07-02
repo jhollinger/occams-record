13477426bb62154548261bcee36d97828f8eb8b4
Jordan Hollinger <jordan.hollinger@wellsky.com>
2021-07-02 19:22:39 -0400

    Remove test AR version output (redundant with apprailsal)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,5 @@ require 'occams-record'
 require 'active_record'
 require 'minitest/autorun'
 Dir.glob('./test/support/*.rb').each { |file| require file }
-puts "Testing against ActiveRecord version #{ActiveRecord::VERSION::MAJOR}.#{ActiveRecord::VERSION::MINOR}"
 TestHelpers.load_fixtures!
 $occams_record_test = true
