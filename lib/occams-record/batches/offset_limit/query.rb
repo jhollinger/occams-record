0bb98c3596b4b9ff8ac21d7a567d67f1a7a9e48f
Jordan Hollinger <jordan.hollinger@wellsky.com>
2022-05-22 01:56:24 -0400

    Refactor details into classes

--- lib/occams-record/batches/offset_limit/query.rb
+++ lib/occams-record/batches/offset_limit/query.rb
@@ -21,7 +21,7 @@ module OccamsRecord
         #
         def find_each(batch_size: 1000, use_transaction: true, append_order_by: nil)
           enum = Enumerator.new { |y|
-            batches(of: batch_size, use_transaction: use_transaction, append_order_by: append_order_by).each { |batch|
+            find_in_batches(batch_size: 1000, use_transaction: use_transaction, append_order_by: append_order_by).each { |batch|
               batch.each { |record| y.yield record }
             }
           }
@@ -47,7 +47,10 @@ module OccamsRecord
         # @return [Enumerator] will yield each batch
         #
         def find_in_batches(batch_size: 1000, use_transaction: true, append_order_by: nil)
-          enum = batches(of: batch_size, use_transaction: use_transaction, append_order_by: append_order_by)
+          enum = Batches
+            .new(model, scope, use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders)
+            .enum(batch_size: batch_size, use_transaction: use_transaction, append_order_by: append_order_by)
+
           if block_given?
             enum.each { |batch| yield batch }
           else
@@ -55,60 +58,67 @@ module OccamsRecord
           end
         end
 
-        private
+        class Batches
+          def initialize(model, scope, use: nil, query_logger: nil, eager_loaders: nil)
+            @model, @scope = model, scope
+            @use, @query_logger, @eager_loaders = use, query_logger, eager_loaders
+          end
 
-        #
-        # Returns an Enumerator that yields batches of records, of size "of".
-        # NOTE ActiveRecord 5+ provides the 'in_batches' method to do something
-        # similiar, although 4.2 does not. Also it does not respect ORDER BY,
-        # whereas this does.
-        #
-        # @param of [Integer] batch size
-        # @param use_transaction [Boolean] Ensure it runs inside of a database transaction
-        # @param append_order_by [String] Append this column to ORDER BY to ensure consistent results. Defaults to the primary key. Pass false to disable.
-        # @return [Enumerator] yields batches
-        #
-        def batches(of:, use_transaction: true, append_order_by: nil)
-          append_order =
-            case append_order_by
-            when false then nil
-            when nil then model.primary_key
-            else append_order_by
-            end
+          #
+          # Returns an Enumerator that yields batches of records, of size "of".
+          # NOTE ActiveRecord 5+ provides the 'in_batches' method to do something
+          # similiar, although 4.2 does not. Also it does not respect ORDER BY,
+          # whereas this does.
+          #
+          # @param batch_size [Integer] batch size
+          # @param use_transaction [Boolean] Ensure it runs inside of a database transaction
+          # @param append_order_by [String] Append this column to ORDER BY to ensure consistent results. Defaults to the primary key. Pass false to disable.
+          # @return [Enumerator] yields batches
+          #
+          def enum(batch_size:, use_transaction: true, append_order_by: nil)
+            append_order =
+              case append_order_by
+              when false then nil
+              when nil then @model.primary_key
+              else append_order_by
+              end
 
-          Enumerator.new do |y|
-            if use_transaction and model.connection.open_transactions == 0
-              model.connection.transaction {
-                run_batches y, of, append_order
-              }
-            else
-              run_batches y, of, append_order
+            Enumerator.new do |y|
+              if use_transaction and @model.connection.open_transactions == 0
+                @model.connection.transaction {
+                  run_batches y, batch_size, append_order
+                }
+              else
+                run_batches y, batch_size, append_order
+              end
             end
           end
-        end
 
-        def run_batches(y, of, append_order_by = nil)
-          limit = scope.limit_value
-          batch_size = limit && limit < of ? limit : of
+          private
 
-          offset = scope.offset_value || 0
-          out_of_records, count = false, 0
-          order_by =
-            if append_order_by
-              append_order_by.to_s == model.primary_key.to_s ? append_order_by.to_sym : append_order_by
+          def run_batches(y, of, append_order_by = nil)
+            limit = @scope.limit_value
+            batch_size = limit && limit < of ? limit : of
+
+            offset = @scope.offset_value || 0
+            out_of_records, count = false, 0
+            order_by =
+              if append_order_by
+                append_order_by.to_s == @model.primary_key.to_s ? append_order_by.to_sym : append_order_by
+              end
+
+            until out_of_records
+              l = limit && batch_size > limit - count ? limit - count : batch_size
+              q = @scope
+              q = q.order(order_by) if order_by
+              q = q.offset(offset).limit(l)
+              results = ::OccamsRecord::Query.new(q, use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders).run
+
+              y.yield results if results.any?
+              count += results.size
+              offset += results.size
+              out_of_records = results.size < batch_size || (limit && count >= limit)
             end
-
-          until out_of_records
-            l = limit && batch_size > limit - count ? limit - count : batch_size
-            q = scope
-            q = q.order(order_by) if order_by
-            q = q.offset(offset).limit(l)
-            results = ::OccamsRecord::Query.new(q, use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders).run
-
-            y.yield results if results.any?
-            count += results.size
-            offset += results.size
-            out_of_records = results.size < batch_size || (limit && count >= limit)
           end
         end
       end
--- lib/occams-record/batches/offset_limit/raw_query.rb
+++ lib/occams-record/batches/offset_limit/raw_query.rb
@@ -22,7 +22,7 @@ module OccamsRecord
         #
         def find_each(batch_size: 1000, use_transaction: true)
           enum = Enumerator.new { |y|
-            batches(of: batch_size, use_transaction: use_transaction).each { |batch|
+            find_in_batches(batch_size: batch_size, use_transaction: use_transaction).each { |batch|
               batch.each { |record| y.yield record }
             }
           }
@@ -47,7 +47,9 @@ module OccamsRecord
         # @return [Enumerator] will yield each batch
         #
         def find_in_batches(batch_size: 1000, use_transaction: true)
-          enum = batches(of: batch_size, use_transaction: use_transaction)
+          enum = Batches
+            .new(conn, @sql, @binds, use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders)
+            .enum(batch_size: batch_size, use_transaction: use_transaction)
           if block_given?
             enum.each { |batch| yield batch }
           else
@@ -55,44 +57,51 @@ module OccamsRecord
           end
         end
 
-        private
-
-        #
-        # Returns an Enumerator that yields batches of records, of size "of".
-        # The SQL string must include 'LIMIT %{batch_limit} OFFSET %{batch_offset}'.
-        # The bind values will be provided by OccamsRecord.
-        #
-        # @param of [Integer] batch size
-        # @param use_transaction [Boolean] Ensure it runs inside of a database transaction
-        # @return [Enumerator] yields batches
-        #
-        def batches(of:, use_transaction: true)
-          unless @sql =~ /LIMIT\s+%\{batch_limit\}/i and @sql =~ /OFFSET\s+%\{batch_offset\}/i
-            raise ArgumentError, "When using find_each/find_in_batches you must specify 'LIMIT %{batch_limit} OFFSET %{batch_offset}'. SQL statement: #{@sql}"
+        class Batches
+          def initialize(conn, sql, binds, use: nil, query_logger: nil, eager_loaders: nil)
+            @conn, @sql, @binds = conn, sql, binds
+            @use, @query_logger, @eager_loaders = use, query_logger, eager_loaders
           end
 
-          Enumerator.new do |y|
-            if use_transaction and conn.open_transactions == 0
-              conn.transaction {
-                run_batches y, of
-              }
-            else
-              run_batches y, of
+          #
+          # Returns an Enumerator that yields batches of records, of size "of".
+          # The SQL string must include 'LIMIT %{batch_limit} OFFSET %{batch_offset}'.
+          # The bind values will be provided by OccamsRecord.
+          #
+          # @param batch_size [Integer] batch size
+          # @param use_transaction [Boolean] Ensure it runs inside of a database transaction
+          # @return [Enumerator] yields batches
+          #
+          def enum(batch_size:, use_transaction: true)
+            unless @sql =~ /LIMIT\s+%\{batch_limit\}/i and @sql =~ /OFFSET\s+%\{batch_offset\}/i
+              raise ArgumentError, "When using find_each/find_in_batches you must specify 'LIMIT %{batch_limit} OFFSET %{batch_offset}'. SQL statement: #{@sql}"
+            end
+
+            Enumerator.new do |y|
+              if use_transaction and @conn.open_transactions == 0
+                @conn.transaction {
+                  run_batches y, batch_size
+                }
+              else
+                run_batches y, batch_size
+              end
             end
           end
-        end
 
-        def run_batches(y, of)
-          offset = 0
-          loop do
-            results = ::OccamsRecord::RawQuery.new(@sql, @binds.merge({
-              batch_limit: of,
-              batch_offset: offset,
-            }), use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders).run
+          private
 
-            y.yield results if results.any?
-            break if results.size < of
-            offset += results.size
+          def run_batches(y, of)
+            offset = 0
+            loop do
+              results = ::OccamsRecord::RawQuery.new(@sql, @binds.merge({
+                batch_limit: of,
+                batch_offset: offset,
+              }), use: @use, query_logger: @query_logger, eager_loaders: @eager_loaders).run
+
+              y.yield results if results.any?
+              break if results.size < of
+              offset += results.size
+            end
           end
         end
       end
