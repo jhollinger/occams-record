25b13d684602a07061d32fbb30c042fa79320175
Jordan Hollinger <jordan.hollinger@wellsky.com>
2022-05-22 22:13:28 -0400

    Add test for cursor.move

--- test/cursor_test.rb
+++ test/cursor_test.rb
@@ -15,17 +15,6 @@ class CursorTest < Minitest::Test
     Time.zone = nil
   end
 
-  def test_cursor
-    return if @skip
-    widgets = OccamsRecord
-      .query(Widget.order("name"))
-      .cursor
-      .open do |cursor|
-        cursor.each(batch_size: 3).to_a
-      end
-    assert_equal Widget.pluck(:name).sort, widgets.map(&:name).sort
-  end
-
   def test_cursor_with_scope
     return if @skip
     widgets = OccamsRecord
@@ -109,5 +98,39 @@ class CursorTest < Minitest::Test
       Bar
       Bar
     ), widgets.map { |w| w.category.name }
+  end
+
+  def test_cursor_each
+    return if @skip
+    widgets = OccamsRecord
+      .query(Widget.order("name"))
+      .cursor
+      .open do |cursor|
+        cursor.each(batch_size: 3).to_a
+      end
+    assert_equal Widget.pluck(:name).sort, widgets.map(&:name).sort
+  end
+
+  def test_cursor_move
+    return if @skip
+    widgets = OccamsRecord
+      .query(Widget.order("name"))
+      .cursor
+      .open do |cursor|
+        res = []
+        res += cursor.fetch(:forward, 3)
+        cursor.move(:backward, 2)
+        res += cursor.fetch(:forward, 3)
+        res
+      end
+
+    assert_equal [
+      "Widget A",
+      "Widget B",
+      "Widget C",
+      "Widget B",
+      "Widget C",
+      "Widget D",
+    ], widgets.map(&:name)
   end
 end
