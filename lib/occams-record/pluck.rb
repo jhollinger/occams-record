871241c1919da46cbe15697112b5f329b17d9700
Jordan Hollinger <jordan.hollinger@procore.com>
2023-07-01 12:23:20 -0400

    Micro-optimizations for #pluck

--- lib/occams-record/pluck.rb
+++ lib/occams-record/pluck.rb
@@ -3,28 +3,40 @@ module OccamsRecord
     private
 
     def pluck_results(results, cols, model: nil)
-      # when we're returning 1 column per result
       if cols.size == 1
-        col = cols[0].to_s
-        enum = model&.defined_enums&.[](col)
-        inv_enum = enum&.invert
-        if enum
-          results.map { |r|
-            val = r[col]
-            enum.has_key?(val) ? val : inv_enum[val]
-          }
-        else
-          results.map { |r| r[col] }
-        end
-      # an array of columns per result
+        pluck_results_single(results, cols[0].to_s, model: model)
       else
-        cols = cols.map { |col|
-          col = col.to_s
-          enum = model&.defined_enums&.[](col)
-          [col, enum, enum&.invert]
+        pluck_results_multi(results, cols.map(&:to_s), model: model)
+      end
+    end
+
+    # returns an array of values
+    def pluck_results_single(results, col, model: nil)
+      enum = model&.defined_enums&.[](col)
+      inv_enum = enum&.invert
+      if enum
+        results.map { |r|
+          val = r[col]
+          enum.has_key?(val) ? val : inv_enum[val]
         }
+      else
+        # micro-optimization for when there are no enums
+        results.map { |r| r[col] }
+      end
+    end
+
+    # returns an array of arrays
+    def pluck_results_multi(results, cols, model: nil)
+      any_enums = false
+      cols_with_enums = cols.map { |col|
+        enum = model&.defined_enums&.[](col)
+        any_enums ||= !!enum
+        [col, enum, enum&.invert]
+      }
+
+      if any_enums
         results.map { |row|
-          cols.map { |(col, enum, inv_enum)|
+          cols_with_enums.map { |(col, enum, inv_enum)|
             if enum
               val = row[col]
               enum.has_key?(val) ? val : inv_enum[val]
@@ -32,6 +44,11 @@ module OccamsRecord
               row[col]
             end
           }
+        }
+      else
+        # micro-optimization for when there are no enums
+        results.map { |row|
+          cols.map { |col| row[col] }
         }
       end
     end
